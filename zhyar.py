0c48db522000524aabb246c6d05316962f16f6ba
zhayr <zhayr@gmial.com>
2025-09-15 16:51:45 +0300

    zhyar axjalary

--- zhyar.py
+++ zhyar.py
@@ -1,9 +1,13 @@
 x = 2
 y = 3
+b = 4
+
 if x == 3:
     print("x rasta")
 
 elif y == 2:
     print("y rasta")
+    elif b == 24:
+    print(" rasta")
 else:
     print("no one of the conditions is true")
