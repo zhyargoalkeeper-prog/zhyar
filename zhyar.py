6a9bed698c11bd5359874a99b92851b28b39e2b1
zhayr <zhayr@gmial.com>
2025-09-15 16:54:05 +0300

    add the a variable to it

--- zhyar.py
+++ zhyar.py
@@ -1,13 +1,12 @@
 x = 2
 y = 3
 b = 4
+a  =3
+
 
 if x == 3:
     print("x rasta")
 
-elif y == 2:
-    print("y rasta")
-    elif b == 24:
-    print(" rasta")
+
 else:
     print("no one of the conditions is true")
